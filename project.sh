4b39a73cfb4b2df4e7b8d532c115ced507d3b7ac
doug <linderd@iinet.net.au>
2015-04-01 10:52:32 +0800

    project

---
+++ project.sh
@@ -0,0 +1,6 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/config.sh
+rm -f $PROJECTS/$1/Binaries/Mac/UE4Editor-$1.dylib
+cd $UE4/UnrealEngine/Engine/Build/BatchFiles/Mac
+./GenerateProjectFiles.sh ${1}Editor Mac Development $PROJECTS/$1/$1.uproject
+$DIR/build.sh $1
