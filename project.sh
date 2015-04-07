b81c7cf0c6eb9b4e0a7145045743746805690a1e
doug <linderd@iinet.net.au>
2015-04-07 23:04:48 +0800

    Make project builder force rebuild of all files

--- project.sh
+++ project.sh
@@ -1,6 +1,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
-rm -f $PROJECTS/$1/Binaries/Mac/UE4Editor-$1.dylib
+rm -f $PROJECTS/$1/Binaries
+rm -f $PROJECTS/$1/Intermediate
 cd $UE4/UnrealEngine/Engine/Build/BatchFiles/Mac
 ./GenerateProjectFiles.sh ${1}Editor Mac Development $PROJECTS/$1/$1.uproject
 $DIR/build.sh $1
