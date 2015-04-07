db9be80cba4b9d7dd966fe0c656300fb938c40b8
doug <linderd@iinet.net.au>
2015-04-07 21:12:10 +0800

    Makes build force rebuild for hot reload

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
-rm -f $PROJECTS/$1/Binaries/Mac/UE4Editor-$1.dylib
+rm -f $PROJECTS/$1/Binaries/Mac/UE4Editor-$1*.dylib
 cd $UE4/UnrealEngine
 ./Engine/Build/BatchFiles/Mac/Build.sh ${1}Editor Mac Development $PROJECTS/$1/$1.uproject
