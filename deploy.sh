cede0d53c337a5ccba75e4a754c2926233745bed
doug <linderd@iinet.net.au>
2015-04-19 21:40:54 +0800

    cleanup of project and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
 mkdir -p $DIST/$1
-$UE4/UnrealEngine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun -nocompileeditor -nop4 -project=$PROJECTS/$1/$1.uproject -cook -allmaps -stage -archive -archivedirectory=$DIST/$1 -package -clientconfig=Development -ue4exe=UE4Editor -clean -pak -prereqs -nodebuginfo -targetplatform=Mac -build -utf8output
+MODE=Shipping
+# MODE=Development
+$UE4/UnrealEngine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun -nocompileeditor -nop4 -project=$PROJECTS/$1/$1.uproject -cook -allmaps -stage -archive -archivedirectory=$DIST/$1 -package -clientconfig=$MODE -ue4exe=UE4Editor -clean -pak -prereqs -nodebuginfo -targetplatform=Mac -build -utf8output
+echo MODE: $MODE
+echo DIST: $DIST/$1
--- project.sh
+++ project.sh
@@ -1,7 +1,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
-rm -f $PROJECTS/$1/Binaries
-rm -f $PROJECTS/$1/Intermediate
+rm -rf $PROJECTS/$1/Binaries
+rm -rf $PROJECTS/$1/Intermediate
 cd $UE4/UnrealEngine/Engine/Build/BatchFiles/Mac
 ./GenerateProjectFiles.sh ${1}Editor Mac Development $PROJECTS/$1/$1.uproject
 $DIR/build.sh $1
