0dc9044c9264ae75d1aac9ab1cffdf6fa698901f
doug <linderd@iinet.net.au>
2015-05-15 12:02:26 +0800

    adds a debug script. Woo~

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
-$EDITOR_EXE $PROJECTS/$1/$1.uproject -Game -ResX=400 -ResY=300
+lldb $EDITOR_EXE -- $PROJECTS/$1/$1.uproject -Game -ResX=200 -ResY=200
