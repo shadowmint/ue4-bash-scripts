6934b3ca2a9f9b7cbe712c6de9e0bb1770f23a29
Douglas Linder <linderd@iinet.net.au>
2016-02-14 15:29:15 +0800

    better buidl and test

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
 cd $UE4/UnrealEngine
-./Engine/Build/BatchFiles/Mac/Build.sh ${1}Editor Mac Development $PROJECTS/$1/$1.uproject
+echo "Building: $PROJECTS/$1/$1.uproject"
+./Engine/Build/BatchFiles/Mac/Build.sh TestProjectEditor Mac Development $PROJECTS/$1/$1.uproject
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/config.sh
-$EDITOR_EXE $PROJECTS/$1/$1.uproject -Game -ExecCmds="Automation RunAll"
+$DIR/build.sh $1
+$EDITOR_EXE $PROJECTS/$1/$1.uproject -Game -ExecCmds="Automation RunTests $1Tests" -unattended -nopause -testexit="Automation Test Queue Empty" -log="TestResults.txt"
+echo "open ~/Library/Logs/$1/TestResults.txt"
